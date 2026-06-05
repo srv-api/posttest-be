f2e2678df84e21c3b652aae0d97bf57f8112431b
srv-kirim <inbox.kirim@gmail.com>
2026-06-05 11:21:23 +0700

    routes & main

--- routes/routes.go
+++ routes/routes.go
@@ -1,71 +1,32 @@
 package routes
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"time"
+	"posttest-be/configs"
+	h_auth "posttest-be/handlers/auth"
+	r_auth "posttest-be/repositories/auth"
+	s_auth "posttest-be/services/auth"
 
-	"posttest-be/entity"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/labstack/echo/v4"
+	"github.com/srv-api/middlewares/middlewares"
 )
 
-type Config struct {
-	DB_Username string
-	DB_Password string
-	DB_Port     string
-	DB_Host     string
-	DB_Name     string
-}
+var (
+	DB  = configs.InitDB()
+	JWT = middlewares.NewJWTService()
 
-func InitDB() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	authR = r_auth.NewAuthRepository(DB)
+	authS = s_auth.NewAuthService(authR, JWT)
+	authH = h_auth.NewAuthHandler(authS)
+)
+
+func New() *echo.Echo {
+
+	e := echo.New()
+
+	auth := e.Group("/auth", middlewares.ApiKeyMiddleware)
+	{
+		auth.POST("/signin", authH.Signin)
 	}
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	// Create the connection string for PostgreSQL
-	dbURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUser, dbName, dbPassword)
-
-	// Open connection to the database
-	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sqlDB, _ := db.DB()
-
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(10)
-
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	sqlDB.SetMaxOpenConns(100)
-
-	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	sqlDB.SetConnMaxLifetime(time.Hour)
-
-	// Migrate the schema
-	db.AutoMigrate(
-		&entity.AccessDoor{},
-	)
-
-	return db
-}
-
-func InitialMigration(db *gorm.DB) {
-	// Get the underlying sql.DB instance from the gorm.DB instance.
-	dbSQL, err := db.DB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dbSQL.Close() // Close the database connection
+	return e
 }
